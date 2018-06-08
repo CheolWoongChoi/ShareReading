be4325448916c1be727541be79bd750f82414f06
CheolWoong <bluesky4381@naver.com>
2018-06-08 19:07:53 +0900

    (Server) MyPage Book 추가 API 작업중

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -3,35 +3,70 @@ const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 const _storage = multer.diskStorage({
-   destination: (req, file, cb) => {
+   
+   destination: (req, file, callback) => {
 
       let path = `./uploads/${req.session.passport.user.nickname}`;
 
-      fs.mkdirSync(path);
-      cb(null, path);
+      if(!fs.existsSync(path))
+         fs.mkdirSync(path);
+      
+      callback(null, path);
+
    },
-   filename: (req, file, cb) => {
-      cb(null, file.originalname);
+   filename: (req, file, callback) => {
+      callback(null, file.originalname);
    }
+
 });
 const upload = multer({ storage: _storage });
 const route = express.Router();
 const conn = require('../config/db')();
 
-route.post('/add', upload.single('bookImage'), (req, res) => {
-   res.send('Uploaded: '+req.file.filename);
-   // let sql = `SELECT nickname FROM users`;
-   // conn.query(sql, (err, result) => {
-   //    if(err){
-   //       console.log(err);
-   //    }
-   //    else{
-   //       //console.log(result);
-   //       res.send(result);
-   //    }
-   // });
+route.get('/books', (req, res) => {
 
+   let nickname = req.session.passport.user.nickname;
+   let sql = `SELECT * FROM books WHERE nickname = ? `;
 
+   conn.query(sql, nickname, (err, result) => {
+      if(err){
+         console.log(err);
+         res.send('Internal Server Error : Fetch Books Error');
+      }
+      else{
+         //console.log(result);
+         res.send(result);
+      }
+   });
 });
+
+route.post('/add', upload.single('bookImage'), (req, res) => {
+   
+   //res.send('Uploaded: '+req.file.filename);
+   let sql = `INSERT INTO books (nickname, bookName, bookImage, author, pubDate, memo) VALUES ?`;
+   let values = [
+                    [ req.session.passport.user.nickname,
+                     req.body.bookName,
+                     req.file.originalname,
+                     req.body.author,
+                     req.body.pubDate,
+                     req.body.memo ]
+                ];
+   
+   conn.query(sql, [values], (err, result) => {
+      if(err){
+         console.log(err);
+         res.send('Internal Server Error : Database Upload Error');
+      }
+      else{
+         //console.log(result);
+         res.redirect('/mypage/add/success');
+      }
+   });
+});
+
+route.get('/add/success', (req, res) => {
+   res.send(true);
+})
 
 module.exports = route
